2af1ece40b8ee46bb7528b6734dab062dc4e0c19
rashadAhmkhan <efruit901@gmail.com>
2026-08-14 06:48:11 +0000

    Michelle Obama

--- day_1/machine/string.py
+++ day_1/machine/string.py
@@ -1,1 +1,11 @@
+#formatted strings
+
+age = 20
+name = "Ben Dover"
+
+print(f"{name} is {age} years old") #makes string formatting easier by removing commas and +'s)
+
+print("C:\Users\newsp\Downloads") #unicode error , use \\ to prevent unicode error
+
+
 
--- day_1/main.py
+++ day_1/main.py
@@ -16,7 +16,7 @@ print("Sum = ", a+b)
 #product 
 total_qty = 35
 price = 60
-revenue = price*quantity
+revenue = price*total_qty
 print(revenue)
 
 #formatted string
@@ -26,4 +26,59 @@ PythonBasics
 Data Engineering
 AI engineering""")
 
+#Read input as text
 
+name = input("Enter Your Name: ")
+age = int(input("enter Age: "))
+
+print("Hello ", name)
+print("You will be ", age+1, "next year")
+
+
+#user-defined fucntions
+
+def sum(a,b):
+	return a+b
+
+a= int(input("A= "))
+b = int(input("B= "))
+sum = sum(a,b)
+
+print(f"Sum = {sum}"
+)
+
+#unicode error
+
+print("C:\Users\newsp\Downloads") #use \\ to prevent
+
+
+#methods replace and split
+
+date = 2026/05/10
+print(date.replace("/","-")
+
+csv = "Manish,25,USA"
+print(csv.split(".")) #gives list of strings
+
+#string navigation
+
+date= "2025/08/10"
+print(date.replace("/","-"))
+
+first = "abhishek"; last= "kumar"
+print(f"{first} {last}")
+
+csv= "abhishek,20,India"
+print(csv.split(","))
+
+print("="*20)
+
+code = "Abhishek-22"
+print(code[0])
+print(code[-1])
+print(code[0:6])
+print(code[-4:])
+date= "2025-08-14"
+print(date[0:4],date[5:7],date[-2:])
+
+print(code[0:10:2])
