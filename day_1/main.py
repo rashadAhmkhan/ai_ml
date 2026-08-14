987a98afcfe0cbb44dd6d04b1e56b5e428d06c13
rashadAhmkhan <efruit901@gmail.com>
2026-08-14 05:47:56 +0000

    added multiple programs

---
+++ day_1/main.py
@@ -0,0 +1,29 @@
+#lists of list
+list  = [[1,2,3,4], ["A","B"]] 
+
+print(list)
+
+
+name = input("Enter name")
+print(name)
+
+print("Lion Eats Grass")
+
+a = 10
+b = 30
+print("Sum = ", a+b)
+
+#product 
+total_qty = 35
+price = 60
+revenue = price*quantity
+print(revenue)
+
+#formatted string
+
+print("""LEarning path:
+PythonBasics
+Data Engineering
+AI engineering""")
+
+
